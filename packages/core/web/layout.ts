5993f3ba5c3e546dd1c86521ba8250226b3bf16d
fservio <fabioservio@gmail.com>
2026-02-05 22:43:26 -0300

    fix: move inline font loading listener to nonced script block (final CSP fix)

--- packages/core/web/layout.ts
+++ packages/core/web/layout.ts
@@ -314,7 +314,12 @@ export function renderPublicLayout(params: PublicLayoutParams): string {
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400&display=swap">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400&display=swap" media="print" onload="this.media='all'">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400&display=swap" media="print" id="google-fonts-link">
+  <script nonce="${nonce}">
+    document.getElementById('google-fonts-link').addEventListener('load', function() {
+      this.media = 'all';
+    });
+  </script>
   <noscript>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400&display=swap">
   </noscript>
